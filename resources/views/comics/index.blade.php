6a793d191c46287eb4970740d8965ad2ae373054
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-06-12 16:34:05 +0200

    add: index of ComicController

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,36 @@
+@extends('comics.layout.main_dashboard')
+
+@section('title_dashboard')
+  Index
+@endsection
+
+@section('content_dashboard')
+  <main>
+    <div class="container my-4">
+      <h1 class="mb-4">Lista dei fumetti</h1>
+
+      <div class="pg-table-wrapper rounded-2 border border-1 shadow-sm overflow-hidden">
+        <table class="table table-light mb-0">
+          <thead>
+            <tr>
+              <th scope="col">#ID</th>
+              <th scope="col">Title</th>
+              <th scope="col">Series</th>
+              <th scope="col">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            @foreach ($comics as $comic)
+            <tr class="vertical-align-middle">
+              <th scope="row">{{$comic->id}}</th>
+              <td>{{$comic->title}}</td>
+              <td>{{$comic->series}}</td>
+              <td><a href="" class="btn btn-primary">Vai</a></td>
+            </tr>
+            @endforeach
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </main>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ComicController;
 use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [PageController::class, 'index'])->name('home');
+
+Route::get('comics', [ComicController::class, 'index'])->name('comics');
