46191f3a65f552a43b9dbbbe334b645965344d7b
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-06-12 15:11:37 +0200

    add: PageController, routes, and views

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layout.main')
+
+@section('title')
+  Home
+@endsection
+
+@section('content')
+  <main>
+    <div class="container">
+      <h1>Home</h1>
+
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo id impedit fuga ipsum, accusamus harum quibusdam ipsam molestias aut, beatae officia quas deserunt! Voluptas facere mollitia eius iure dolore, fugiat ad voluptatem illo ab fuga pariatur ipsum ea. Quaerat architecto dignissimos iusto blanditiis ipsa necessitatibus dolor, ducimus laudantium illo? Tenetur eum facere eaque laboriosam explicabo! Iusto optio necessitatibus dolor reiciendis ipsa, ducimus distinctio quod illo accusantium architecto est odit, voluptatum adipisci dolore. Dolores magni, alias vero nostrum quidem eveniet voluptas suscipit explicabo, animi facere natus rerum exercitationem aperiam esse ad quos, accusantium quis vel sit? Et praesentium dolor repellat unde?</p>
+    </div>
+  </main>
+@endsection
