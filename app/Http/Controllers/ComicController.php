cafd86ad92131bf5a3ea0d15f6daf47e7b9c6bc9
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-06-13 18:00:28 +0200

    add: comic update in database

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -69,8 +69,10 @@ class ComicController extends Controller
      * @param  \App\Models\Comic  $comic
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Comic $comic){
-      //
+    public function update(ComicRequest $request, Comic $comic){
+      $form_data = $request->all();
+      $comic->update($form_data);
+      return redirect()->route('comics.index');
     }
 
     /**
